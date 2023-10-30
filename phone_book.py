636768005a404be13edeecc5a808b8a134c92ea9
JustCheka <cheka201@yandxe.ru>
2023-10-30 21:56:53 +0300

    Second version

--- phone_book.py
+++ phone_book.py
@@ -12,14 +12,14 @@ def show_menu():
 def find_by_lastname(phone_book,last_name):
     for i in range(len(phone_book)):
         nowPhone = phone_book[i]
-        if (last_name in nowPhone['Фамилия']):
+        if (last_name == nowPhone['Фамилия']):
             return "Телефон: " + nowPhone['Телефон']
     return "Такой фамилии нет или фамилия написана неправильно (запись устойчива к регистру)"
 
 def change_number(phone_book,last_name,new_number):
     for i in phone_book:
         nowPhone = i
-        if (last_name in nowPhone['Фамилия']):
+        if (last_name == nowPhone['Фамилия']):
             nowPhone['Телефон'] = new_number
             return nowPhone
     return "Такой фамилии нет или фамилия написана неправильно (запись устойчива к регистру)"
@@ -27,11 +27,17 @@ def change_number(phone_book,last_name,new_number):
 def delete_by_lastname(phone_book,lastname):
     for i in range(len(phone_book)):
         nowPhone = phone_book[i]
-        if (lastname in nowPhone['Фамилия']):
+        if (lastname == nowPhone['Фамилия']):
             phone_book.pop(i)
             return "Успешно удалено"
     return "Такой фамилии нет или фамилия написана неправильно (запись устойчива к регистру)"
     
+def find_by_number(phone_book,number):
+    for i in range(len(phone_book)):
+        nowPhone = phone_book[i]
+        if (number == nowPhone['Телефон']):
+            return nowPhone
+    return "Такого телефона нет"
 
 def read_txt(filename):
 
@@ -43,30 +49,40 @@ def read_txt(filename):
         for line in phb:
             if (len(line) > 1):
                 line = line.replace("\t", "").replace("\n", " ")
-                record = dict(zip(fields, line.split(',')))               
+                record = dict(zip(fields, line.split(',')))        
+                record['Фамилия'] = record['Фамилия'].replace(" ", "") 
+                record['Имя'] = record['Имя'].replace(" ", "")
+                record['Телефон'] = record['Телефон'].replace(" ", "")
                 phone_book.append(record)
+
+    return phone_book
+
+def add_user(phone_book,user_data):
+    fields=['Фамилия', 'Имя', 'Телефон', 'Описание']
+    line = user_data.replace("\t", "").replace("\n", " ")
+    record = dict(zip(fields, line.split(',')))               
+    phone_book.append(record)
 
     return phone_book
 
 def write_txt(filename , phone_book):
 
-    with open('phonebook.txt','w',encoding='utf-8') as phout:
+    with open(filename,'w',encoding='utf-8') as phout:
 
         for i in range(len(phone_book)):
 
             s=''
             for v in phone_book[i].values():
                 s+=v+','
-                phout.write(f'{s[:-1]}\n')
+            phout.write(f'{s[:-1]}\n')
+
 
 def work_with_phonebook():
     choice=show_menu()
-
-    phone_book = read_txt('111.txt')
+    phone_book = read_txt('phone_book.txt')
     
     while (choice!=7):
-
-        if choice==1:          
+        if choice==1:
             for i in range(len(phone_book)):
                 print(i + 1, f" {phone_book[i]}")
             print()
@@ -84,12 +100,12 @@ def work_with_phonebook():
             print(delete_by_lastname(phone_book,lastname))
             print()
         elif choice==5:
-            number=input('number ')
-            #print(find_by_number(phone_book,number))
+            number = input('number ')
+            print(find_by_number(phone_book,number))
         elif choice==6:
             user_data=input('new data ')
-            #add_user(phone_book,user_data)
-            #write_txt('111.txt',phone_book)
+            phone_book = add_user(phone_book,user_data)
+            write_txt('phone_book.txt',phone_book)
         choice=show_menu()
 
 work_with_phonebook()
